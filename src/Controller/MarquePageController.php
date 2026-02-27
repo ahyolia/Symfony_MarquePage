8713ba4898c891954a80c034fb2292e1a9e3f179
Amin Handoyo Camélia <camhdyo@gmail.com>
2026-02-27 14:58:12 +1100

    AV3
    Page de détails d'un marque-page et groupement des routes/url

--- src/Controller/MarquePageController.php
+++ src/Controller/MarquePageController.php
@@ -8,21 +8,24 @@ use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Doctrine\ORM\EntityManagerInterface;
 
+#[Route("/MarquePage", name: "marquepage_")]
 class MarquePageController extends AbstractController
 {
-    #[Route("/MarquePage/page")]
-    public function fct(EntityManagerInterface $entityManager): Response {
+    #[Route("/", name: "index")]
+    public function index(EntityManagerInterface $entityManager): Response {
 
         $marquepages = $entityManager
-            ->getRepository(MarquePage::class)
-            ->findAll();
+        ->getRepository(MarquePage::class)
+        ->findAll();
             
-        return $this->render('MarquePage/page.html.twig', [
-        'marquepages' => $marquepages,
-    ]);
+        return $this->render('MarquePage/index.html.twig', [
+            'marquepages' => $marquepages
+        ]);
+
+        
     }
 
-    #[Route("/MarquePage/ajouter", name:  "marquepage_ajouter")]
+    #[Route("/ajouter", name: "ajouter")]
     public function ajouter(EntityManagerInterface $entityManager) : Response {
        // Création du marque-page Symfony
        $symfony = new MarquePage();
@@ -51,6 +54,24 @@ class MarquePageController extends AbstractController
        // Retour après avoir tout sauvegardé
        return new Response("3 marque-pages sauvegardés avec succès !");
     }
+
+    #[Route("/details/{id<\d+>}", name: "details")]
+    public function details(int $id, EntityManagerInterface $entityManager): Response {
+        $marquepage = $entityManager
+        ->getRepository(MarquePage::class)
+        ->find($id);
+        
+        if (!$marquepage) {
+            throw $this->createNotFoundException("Marque-page avec l'id $id non trouvé");
+        }
+
+        return $this->render('MarquePage/details.html.twig', [
+            'marquepage' => $marquepage
+        ]);
+    }
+
+    
+
 
 }
 
